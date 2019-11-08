cef47c3e1cc35296f165143734a9713a0fe82187
LeonidasVarveropoulos <leonidas@vrhome.net>
2019-11-08 00:46:13 -0600

    added depth to laser and mapping

--- scripts/robot_ws.sh
+++ scripts/robot_ws.sh
@@ -8,4 +8,6 @@ sleep 5
  
 # Start realsense
 roslaunch robot_urdf rviz_model.launch &
-roslaunch pose_ekf pose_ekf.launch
+roslaunch pose_ekf pose_ekf.launch &
+roslaunch depth_to_laser depth_to_laser.launch &
+rosrun gmapping slam_gmapping scan:=scan
