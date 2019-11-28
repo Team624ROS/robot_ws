40e3094c42669b0676ab83051517930a96cdc4e5
LeonidasVarveropoulos <leonidas@vrhome.net>
2019-11-27 20:54:41 -0600

    updated

--- scripts/robot_ws.sh
+++ scripts/robot_ws.sh
@@ -5,11 +5,11 @@ source ~/robot_ws/devel/setup.bash
 # Start roscore and wait till its finished
 roscore -p 11311 &
 sleep 5
- 
+
 # Start realsense
 roslaunch robot_urdf rviz_model.launch &
 roslaunch pose_ekf pose_ekf.launch &
-roslaunch depth_to_laser depth_to_laser.launch &
+roslaunch depth_to_laser depth_to_laser.launch & # MAKE SURE TO AGJUST SAMPLE OF DEPTH BASED ON HIEGHT OF CAM
 roslaunch merge_laser d435_mod.launch &
 sleep 5
 roslaunch merge_laser merge_laser.launch & 
--- scripts/sim_robot_ws.sh
+++ scripts/sim_robot_ws.sh
@@ -6,8 +6,8 @@ source ~/robot_ws/devel/setup.bash
 roscore -p 11311 &
 sleep 1
 
-#roslaunch mapping mapping_sim.launch
+#roslaunch mapping mapping.launch
 
-roslaunch mapping map_server_sim.launch & # Change file directories for your computer
-roslaunch localization amcl_sim.launch & 
-roslaunch pathfinding move_base_sim.launch
+roslaunch mapping map_server.launch & # Change file directories for your computer
+roslaunch localization amcl.launch 
+#roslaunch pathfinding move_base.launch
